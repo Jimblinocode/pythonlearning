3082807de7aaaafc5da1763d9d39a95cee79a22a
jimblinocode <gorillaking36@gmail.com>
2022-11-26 17:58:03 -0600

    add two numbers

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("helloworld")
+#religeon and politics often make some people lose all perspective and give way to ranting and raving
+print("enter two numbers")
+myint = input()
+myint2 = input()
+
+print(int(myint) + int(myint2))
