95c7924c54e969cb603c3510d349021b1248d40c
jimblinocode <gorillaking36@gmail.com>
2022-12-07 10:48:25 -0600

    if statement practice, made the bodyshamer 9000

---
+++ practice.py
@@ -0,0 +1,12 @@
+
+# made a program that judges you based of your penis size to practice if statements, pretty good use of my time tbh
+
+print("what is your dick size? be honest")
+
+com = input()
+ideal = 6
+
+if int(com) <= ideal:
+    print("haha you get no bitches, do you? you fucking soyboy. LOOOOSER")
+if int(com) > ideal:
+    print("based")
