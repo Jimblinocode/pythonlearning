fb605e2a73241172415edb3e116857953943a741
jimblinocode <gorillaking36@gmail.com>
2022-12-06 20:09:45 -0600

    learned about if statements

---
+++ lesson 2.py
@@ -0,0 +1,19 @@
+# Lesson 2
+
+
+var = True
+var2 = False
+final = 69
+
+if var and var2:
+    final = final + 420
+
+print(final)
+num = 69
+
+if False:
+    pass
+elif True:
+    pass
+else:
+    pass
