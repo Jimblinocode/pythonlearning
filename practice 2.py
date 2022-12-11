00b2ffcdb9fa90c8dd02e0f22c561d89adb5dd04
jimblinocode <gorillaking36@gmail.com>
2022-12-11 11:39:05 -0600

    Challenge 1

---
+++ practice 2.py
@@ -0,0 +1,16 @@
+# lesson 3 Challenge 1: make a program that will keep asking the user for specific input until the user types "stop" or "exit"
+
+print("Please enter a string")
+
+end = "exit" 
+end2 = "stop"
+com = input()
+
+while not (com == end or com == end2):
+    print("Please enter a string")
+    com = input()
+    
+        
+
+    
+    
