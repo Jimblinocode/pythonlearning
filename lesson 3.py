7dee34eda177d462c62dbaf3fb0f624e5b555122
jimblinocode <gorillaking36@gmail.com>
2022-12-10 19:29:28 -0600

    learned about loops and iterables

---
+++ lesson 3.py
@@ -0,0 +1,25 @@
+
+# mytuple = (2, 4, 6)
+
+# mytuple[0] = 3
+
+
+# print(mytuple)
+
+# mylist = [69, 420.9, False, "amogus"]
+
+# mylist.insert(2, "sus")
+
+# print(mylist)
+
+#T = 0
+
+#while T < 1:
+#    print("i have big juicy nipples")
+#    T = T + 1
+
+mylist = ["sus", "amogus", 2, 1000, True]
+
+
+for item in range(40): 
+    print("you are " + str(item))
