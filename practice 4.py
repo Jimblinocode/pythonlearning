7b561321d9dc92753f965d05c13a1e8feacaf3e2
jimblinocode <gorillaking36@gmail.com>
2022-12-14 15:39:26 -0600

    improved efficiency

--- practice 4.py
+++ practice 4.py
@@ -4,19 +4,20 @@ from random import randint
 
 print("Pick a number between 0 and 100")
 
-Num = input()
+
 minimum = 0
 maximum = 100
 ans = randint(0, 100)
 running = True
 
 while running:
-    if int(Num) < minimum or int(Num) > maximum:
+    Num = int(input())
+    if Num < minimum or Num > maximum:
         print("invalid input: must be a number between 0 and 100")
-        Num = input()
-    elif int(Num) > ans or int(Num) < ans:
-        print("WRONG")
-        Num = input()
-    elif int(Num) == ans:
+    elif Num > ans:
+        print("LOWER")
+    elif Num < ans:
+        print("HIGHER")
+    else:
         print("CORRECT!!")
         running = False
