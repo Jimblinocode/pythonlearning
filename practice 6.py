c22919b3bb3bc2ec6aff0199a1219a178fad8c36
jimblinocode <gorillaking36@gmail.com>
2022-12-18 14:44:53 -0600

    fixed some things

--- practice 6.py
+++ practice 6.py
@@ -4,7 +4,7 @@ print("you have 200G.")
 print("There is Nothing in your inventory")
 
 shop = {
-"the Grass Sword of Thesophelese": 50, 
+"The Grass Sword of Thesophelese": 50, 
 "Spell: Finger Gun": 75, 
 "Medium Health Potion": 25,
 "The Magnum Dong Dildo Sword": 200,
@@ -15,30 +15,49 @@ pgold = 200
 
 
 
-print("Welcome to the fantasy battle imporium! your one stop shop for everything legendary swords, powerful spells, thaumically powered appliances and Potions!")
-print("we have...")
-
-
 while running:
+    print("Welcome to the fantasy battle imporium! your one stop shop for everything legendary swords, powerful spells, thaumically powered appliances and Potions!")
+    print("we have...")
+
     for key in shop:
         print(key + "     " + str(shop[key]) + "G")
     com = input()
     
-    for key in shop:
-        if com == key:
-            if pgold < shop[key]:
-                print("Sorry, you can't afford this item.")
-            else:
-                pgold-= shop[key]
-                print("-" + str(shop[key]) + "G")
-                inventory.append(key)
-                print(key + " has been added to inventory")
-                print("you have " + str(pgold)+ "G" + " left")
-                
-            
-    for item in inventory:
-        print("inventory:")
-        print(item)
-        print("------------")
+    if com != key:
+        print("Sorry, we don't have that")
+    if com == "exit":
+        run_exit = True
+        while run_exit:
+            print("you have " + str(pgold) + "G")
+            print("inventory:")
+            for item in inventory:
+                print(item)
+                print("------------")
+            print("Are you sure you want to leave the shop? (Y/N)")
+            com = input()      
+            if com == "yes":
+                running = False
+                run_exit = False
+            elif com == "no":
+                run_exit = False
+    
+    else:
+        for key in shop:
+            if com == key:
+                if pgold < shop[key]:
+                    print("Sorry, you can't afford this item.")
+                    print("----------------------------------")
+                else:
+                    pgold-= shop[key]
+                    print("-" + str(shop[key]) + "G")
+                    inventory.append(key)
+                    print(key + " has been added to inventory")
+                    print("you have " + str(pgold)+ "G" + " left")
+                    print("inventory:")
+                    for item in inventory:
+                        print(item)
+                        print("------------")
+
+
     
      
