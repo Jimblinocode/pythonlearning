a1e83044d0281b4711d72e9f9fe5bb0047dee296
jimblinocode <gorillaking36@gmail.com>
2022-12-17 22:02:41 -0600

    made it 1000x better

--- practice 6.py
+++ practice 6.py
@@ -3,61 +3,42 @@
 print("you have 200G.")
 print("There is Nothing in your inventory")
 
-shop = ["the Grass Sword of Thesophelese 50G", "Spell: Finger Gun              75G", "Medium Health Potion           25G", "The Magnum Dong Dildo Sword   200G", "Exit Shop" ]
+shop = {
+"the Grass Sword of Thesophelese": 50, 
+"Spell: Finger Gun": 75, 
+"Medium Health Potion": 25,
+"The Magnum Dong Dildo Sword": 200,
+}
 running = True
 inventory = []
 pgold = 200
 
+
+
 print("Welcome to the fantasy battle imporium! your one stop shop for everything legendary swords, powerful spells, thaumically powered appliances and Potions!")
 print("we have...")
 
-for item in shop:
-    print(item)
 
 while running:
+    for key in shop:
+        print(key + "     " + str(shop[key]) + "G")
     com = input()
     
-    if pgold == 0:
-        print("sorry, you can't afford that")
+    for key in shop:
+        if com == key:
+            if pgold < shop[key]:
+                print("Sorry, you can't afford this item.")
+            else:
+                pgold-= shop[key]
+                print("-" + str(shop[key]) + "G")
+                inventory.append(key)
+                print(key + " has been added to inventory")
+                print("you have " + str(pgold)+ "G" + " left")
+                
+            
+    for item in inventory:
+        print("inventory:")
+        print(item)
+        print("------------")
     
-    elif com == "The Grass Sword of Thesophelese" or com == "Grass Sword of Thesophelese" or com == "Grass Sword":
-        pgold-= 50
-        inventory.append("Grass Sword of Thesophelese")
-        print("The Grass sword of Thesophelese has been moved to your inventory -50G.")
-        print("you have " + str(pgold),"Gold")
-        print("Inventory:")
-        for item in inventory:
-            print(item)
-    
-    elif com == "Spell: Finger Gun" or com == "Finger Gun":
-        pgold-= 75
-        inventory.append("Spell: Finger Gun")
-        print("a Finger Gun spell has been moved to your inventory -75G.")
-        print("you have " + str(pgold),"Gold")
-        print("Inventory:")
-        for item in inventory:
-            print(item)
-    
-    elif com == "Medium Health Potion" or com == "Health Potion":
-        pgold-= 25
-        inventory.append("Medium Health Potion")
-        print("a Medium Health Potion has been moved to your inventory -25G")
-        print("you have " + str(pgold), "Gold")
-        print("Inventory:")
-        for item in inventory:
-            print(item)
-    
-    elif com == "The Magnum Dong Dildo Sword" or com == "Magnum Dong Dildo Sword" or com == "Dildo Sword":
-        pgold-= 200
-        inventory.append("Magnum Dong Dildo Sword")
-        print("The Magnum Dong Dildo Sword has been Moved to your inventory -200G")
-        print("you have " + str(pgold), "Gold")
-        print("Inventory:")
-        for item in inventory:
-            print(item)
-    
-    elif com == "Exit Shop" or com == "exit shop":
-        running = False
-
-    else:
-        print("INVALID COMMAND")
+     
