37e8e61abdad08193cafd087a648648416bef984
jimblinocode <gorillaking36@gmail.com>
2022-12-20 17:16:05 -0600

    made a thing

--- practice 7.py
+++ practice 7.py
@@ -2,4 +2,16 @@
 
 print("Please enter 10 numbers:")
 
-com = input()
+numlist = []
+
+for i in range(10):
+    com = input()
+    numlist.append(com)
+
+larnum = 0
+for item in numlist:
+    if int(item) > larnum:
+        larnum = int(item)
+print("The largest number is " + str(larnum))
+    
+    
