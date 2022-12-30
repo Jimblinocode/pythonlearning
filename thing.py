544203329bc1be3a30e92e54f015534d7941f5e3
jimblinocode <gorillaking36@gmail.com>
2022-12-29 18:37:40 -0600

    just my older siblings thing i tested

---
+++ thing.py
@@ -0,0 +1,23 @@
+print("Please enter a weapon.")
+weapon = input()
+print("Please enter an animal.")
+animal = input()
+print("Please enter a name.")
+name2 = input()
+print("Please enter your favorite location.")
+coolplace = input()
+print("Please enter an adjective.")
+adjective = input()
+print("Please enter another name.")
+name1 = input()
+print("Please enter a phrase (in all caps) that you would say to calm someone down.")
+phrase = input()
+print("Please enter a trap.")
+trap = input()
+print("Please enter a violent verb.")
+verb = input()
+
+print("Once upon a time, a based and " + adjective + "-pilled " + animal + " named " + name1 + " decided he wanted to prank his friend, " + name2 + """ the spider, for his YouTube channel. To accomplish
+ this prank, he needs a """ + weapon + " and a " + trap + ". " "He aquires these materials and then he travels to " + name2 + "'s favorite place, " + coolplace + """, where he sets up
+  the """ + trap + " and the camera. " + name1 + " lies in wait, readying his " + weapon + " for just the right moment. As " + name2 + """ the spider enters the room,
+  he gets caught in the """ + trap + ". " + name1 + " charges at " + name2 + " with the " + weapon + " in hand, " + verb + "ing him over and over, all the while shouting " + phrase + " at the poor disfigured spider.")
