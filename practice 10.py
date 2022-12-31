b953eaade92fbf71a7d09926e77d88fb0d1ca94c
jimblinocode <gorillaking36@gmail.com>
2022-12-30 20:28:18 -0600

    started the challenge, will finish later

---
+++ practice 10.py
@@ -0,0 +1,13 @@
+# Fibonacci sequence challenge 
+
+# driver code
+
+print("What is the size of your fibonacci sequence?")
+
+com = int(input)
+
+def Fibonnaci(size):
+    fiblist = [0, 1]
+    fib = fiblist[0] + fiblist[1]
+    for item in fiblist:
+        pass
