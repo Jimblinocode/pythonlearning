d7d52738a21ff31ac6835bfaae6de403dcc492ee
jimblinocode <gorillaking36@gmail.com>
2023-01-05 21:59:12 -0600

    e

--- mintosec.py
+++ mintosec.py
@@ -1,5 +1,19 @@
 
+
+# print("which do you want to do")
+
+# comlist = ["1. minutes to seconds", "2. hours to seconds", "3. days to seconds"]
+
+# for item in comlist:
+    # print(item)
+
+
 com = int(input())
+com2 = int(input())
+com3 = int(input())
+com4 = int(input())
+
+
 
 def mintosec(min):
     sec = min * 60
@@ -14,8 +28,17 @@ def daytosec(day):
     return(sec)
 
 def anytosec (any):
-    pass
+    sec = daytosec(com2) + hourtosec(com3) + mintosec(com4) + any
+    return(sec)
 
-print(daytosec(com))
+print(anytosec(com))
 
+# if com == "1":
+    # print("how many minutes?")
+    # com2 = int(input())
+    # print(mintosec(com2))
 
+# elif com == "2":
+    # print("how many hours?")
+    # com2 = int(input())
+    # print(hourtosec(com2))
