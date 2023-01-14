2b10a70066f826243e31ba543cd06cfc079a0ddd
jimblinocode <gorillaking36@gmail.com>
2023-01-14 16:55:49 -0600

    started other challenge

---
+++ practice 12.py
@@ -0,0 +1,14 @@
+shopdict = {
+    "pizza": 6.99,
+    "shake": 4.99,
+    "skittles": 0.99,
+    "special ayylmao weed": 68.99
+}
+
+def snackshop():
+    for key in shopdict:
+        print(key + " ------ " +"$"+ str(shopdict[key]),"\n-- -- -- -- -- -- -- ")
+    return
+    
+
+print(snackshop())
