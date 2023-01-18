bf6edef5d202a725ffd5d562746d9234cebebbce
jimblinocode <gorillaking36@gmail.com>
2023-01-17 19:26:55 -0600

    E

---
+++ lesson X.py
@@ -0,0 +1,34 @@
+# for char in "hello":
+    # print(char)
+
+# immutable: data that can't be changed (strings, floats, integers)
+
+# mutable: data that can be changed (lists, dictionaries, etc.)
+
+# some methods change data while others make new data
+
+# functions can only make new data
+
+# break: a command that will stop the innermost loop
+
+# continue: skips a specific iteration in a loop when the loop hits it
+
+# format strings: typing "f" before a string to add a variable, removes the need for typecasting, also easier to read
+
+# funnyword = input()
+
+# print(f"you are a {funnyword} ")
+
+# explicit typing vs implicit typing
+
+# explicit typing: code where you need to specify the data type in a varibale  
+
+# implicit typing : code where you don't need to specify the data type in a variable 
+
+# dynamically typed vs statically typed
+
+# dynamic typing: the data type of a variable can be changed later in memory
+
+# static typing: the data type of a variable CANNOT be changed later in memeory
+
+# the advantage of explicit typing is the ability to see the options for methods and specify what a functions return
