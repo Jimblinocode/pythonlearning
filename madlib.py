e859d984d44dada0ed9e6b4ac4df75adf29fe951
jimblinocode <gorillaking36@gmail.com>
2023-01-18 02:22:30 -0600

    fuck

--- madlib.py
+++ madlib.py
@@ -1,22 +1,51 @@
 # print("\tpee pee poo poo")
-# print(f"My name is {Name1}. I live at {Address}.\n This is my confession. If you're watching this tape, I'm probably {lifestate}, {verbed} by my {familialrelation},\n {fName} {lName}. {fname} has been building a {noun} empire for over a year now and using me as his\n {jobtitle}. Shortly after my {ageplural} birthday, {fname} came to me with a rather, shocking proposition. He\n asked that I use my {jobtitle} knowledge to {adjective} {noun2}, which he would then {verb} using\n his connections in the {rnoun} world. Connections that he made through his career with the {organization}. I was...\n {emotiveverb}, I... I always thought that {fname} was a very {adjective2} {Gender} and I was... {ano-adjective}, {adjective3}, but I\n was also particularly {adjective4} at the time, something he knew and took advantage of. I was {verbing}\n from a {life-event} that was poised to {adjective5} my family. {fname} took me on a {activity}, and\n showed me just how much {noun3} even a small {noun} operation could make. And I was {adjective6}. I {didordidnt}\n want my family to {potential-event} so I agreed. Every day, I think back at that moment with {emotion}.\n I quickly realized that I was {idiom}, and {fname} had a partner, a man named {fname2}\n {lname2}, a {jobtitle2}. {fname} essentially {verbed2} me into {noun4} to this {Gender2}, and when I tried to quit,\n Fring threatened my family. I didn't know where to turn. Eventually, Hank and Fring had a falling out.\n From what I can gather, Hank was always pushing for a greater share of the business, to which Fring\n flatly refused to give him, and things escalated. Fring was able to arrange, uh I guess I guess you call it\n a hit on my brother-in-law, and failed, but Hank was seriously injured, and I wound up paying his\n medical bills which amounted to a little over $177,000. Upon recovery, Hank was bent on revenge,\n working with a man named Hector Salamanca, he plotted to kill Fring, and did so. In fact, the bomb\n that he used was built by me, and he gave me no option in it. I have often contemplated suicide, but\n I'm a coward. I wanted to go to the police, but I was frightened. Hank had risen in the ranks to become\n the head of the Albuquerque DEA, and about that time, to keep me in line, he took my children from\n me. For 3 months he kept them. My wife, who up until that point, had no idea of my criminal activities,\n was horrified to learn what I had done, why Hank had taken our children. We were scared. I was in\n Hell, I hated myself for what I had brought upon my family. Recently, I tried once again to quit, to end\n this nightmare, and in response, he gave me this. I can't take this anymore. I live in fear every day that\n Hank will kill me, or worse, hurt my family. I... All I could think to do was to make this video in hope\n that the world will finally see this man, for what he really is."")
+# print(f"My name is {Name1}. I live at {Address}.\n 
+# This is my confession. If you're watching this tape, I'm probably {lifestate}, {verbed} by my {familialrelation},\n
+#  {fName} {lName}. {fname} has been building a {noun} empire for over a year now and using me as {pronoundet}\n
+#  {jobtitle}. Shortly after my {ageplural} birthday, {fname} came to me with a rather, shocking proposition. {pronounid}\n
+#  asked that I use my {jobtitle} knowledge to {adjective} {noun2}, which {pronounid} would then {verb} using\n
+#  {pronoundet} connections in the {rnoun} world. Connections that {pronounid} made through {pronoundet} career with the {organization}. I was...\n
+#  {emotiveverb}, I... I always thought that {fname} was a very {adjective2} {Gender} and I was... {ano-adjective}, {adjective3}, but I\n
+#  was also particularly {adjective4} at the time, something {pronounid} knew and took advantage of. I was {verbing}\n
+#  from a {life-event} that was poised to {adjective5} my family. {fname} took me on a {activity}, and\n
+#  showed me just how much {noun3} even a small {noun} operation could make. And I was {adjective6}. I {didordidnt}\n
+#  want my family to {potential-event} so I agreed. Every day, I think back at that moment with {emotion}.\n
+#  I quickly realized that I was {idiom}, and {fname} had a partner, a {Gender2} named {fname2}\n
+#  {lname2}, a {jobtitle2}. {fname} essentially {verbed2} me into {noun4} to this {Gender2}, and when I tried to {verb2},\n
+#  {lname2} {verbed3} my family. I didn't know where to turn. Eventually, {fname1} and {lname2} had a falling out.\n
+#  From what I can gather, {fname1} was always pushing for a greater {Noun5} of the {jobtitle2r}, to which {lname2}\n
+#  flatly refused to give {pronounobj}, and things escalated. {lname2} was able to arrange, uh I guess I guess you call it\n a {verb3} on my {familialrelation}, and failed, but {fname1} was {adverb} {adjective7}, and I wound up {verbing2} {pronoundet}\n
+#  {noun6} which {effect}. Upon recovery, {fname1} was bent on revenge,\n
+#  working with a {Gender3} named {fname3} {lname3}, {pronounid2} plotted to {verb4} {lname2}, and did so. In fact, the {noun7}\n
+#  that {pronounid2} used was built by me, and {pronounid2} gave me no option in it. I have often contemplated {verb5}, but\n
+#  I'm a {adjective8}. I wanted to go to the {noun8}, but I was {emotion2}. {fname1} had risen in the ranks to become\n
+#  the head of the {organization}, and about that time, to keep me in line, {pronounid} took my {nounplural} from\n 
+#  me. For {timeperiod} {pronounid} kept them. My {noun9}, who up until that point, had no idea of my {careertitle},\n
+#  was {emotion3} to learn what I had done, why {fname1} had taken our {nounplural}. We were {emotion3alt}. I was in\n 
+#  {noun10}, I {verbed} myself for what I had {verbed}. Recently, I tried once again to {verb}, to end\n
+#  this {noun11}, and in response, {pronounid} gave me this. I {canorcan't} take this anymore. I live in {emotion4} every day that\n 
+#  {fname1} will {verb} me, or {adjective}, {verb} my family. I... All I could think to do was to make this video in hope\n
+#  that the world will finally see this {Gender}, for what he really is."")
 # variables[
     # Name1
     # Address
     # lifestate
-    # verbed (1,2)
+    # verbed (1,2,3)
     # familialrelation
-    # fname (1,2)
-    # lname (1,2)
-    # noun (1,2,3)
-    # jobtitle (1,2)
+    # fname (1,2,3)
+    # lname (1,2,3)
+    # pronoundet 
+    # pronounid (1,2)
+    # pronounobj
+    # noun (1,2,3,4,5,6,7,8)
+    # jobtitle (1,2,2r)
     # ageplural
-    # adjective (1,2,3,4,5,6) 
-    # verb (1)
+    # adjective (1,2,3,4,5,6,7,8) 
+    # verb (1,2,3,4)
     # rnoun
     # organization
     # emotiveverb
-    # gender (1,2)
+    # gender (1,2,3)
     # ano-adjective
     # verbing
     # life-event
@@ -25,8 +54,8 @@
     # potential-event
     # emotion
     # idiom
-    # ]
-options = ["1. Walter White confession tape", "2. Vaporeon Copypasta", "3. Navy Seal Copypasta",]
+    # emotion]
+options = ["1. Walter White confession tape", "2. Vaporeon Copypasta", "3. Navy Seal Copypasta", "4. Rick and Morty Copypasta", ]
 
 print("Welcome to copypasta generator! which copypasta would you like to generate?")
 
@@ -44,4 +73,106 @@ while running:
     if com == "1 default" or "1":
         print(" My name is Walter Hartwell White. I live at 308 Negra Arroyo Lane, Albuquerque, New Mexico, 87104.\n This is my confession. If you're watching this tape, I'm probably dead, murdered by my brother-in-law\n Hank Schrader. Hank has been building a meth empire for over a year now and using me as his\n chemist. Shortly after my 50th birthday, Hank came to me with a rather, shocking proposition. He\n asked that I use my chemistry knowledge to cook methamphetamine, which he would then sell using\n his connections in the drug world. Connections that he made through his career with the DEA. I was...\n astounded, I... I always thought that Hank was a very moral man and I was... thrown, confused, but I\n was also particularly vulnerable at the time, something he knew and took advantage of. I was reeling\n from a cancer diagnosis that was poised to bankrupt my family. Hank took me on a ride along, and\n showed me just how much money even a small meth operation could make. And I was weak. I didn't\n want my family to go into financial ruin so I agreed. Every day, I think back at that moment with regret.\n I quickly realized that I was in way over my head, and Hank had a partner, a man named Gustavo\n Fring, a businessman. Hank essentially sold me into servitude to this man, and when I tried to quit,\n Fring threatened my family. I didn't know where to turn. Eventually, Hank and Fring had a falling out.\n From what I can gather, Hank was always pushing for a greater share of the business, to which Fring\n flatly refused to give him, and things escalated. Fring was able to arrange, uh I guess I guess you call it\n a hit on my brother-in-law, and failed, but Hank was seriously injured, and I wound up paying his\n medical bills which amounted to a little over $177,000. Upon recovery, Hank was bent on revenge,\n working with a man named Hector Salamanca, he plotted to kill Fring, and did so. In fact, the bomb\n that he used was built by me, and he gave me no option in it. I have often contemplated suicide, but\n I'm a coward. I wanted to go to the police, but I was frightened. Hank had risen in the ranks to become\n the head of the Albuquerque DEA, and about that time, to keep me in line, he took my children from\n me. For 3 months he kept them. My wife, who up until that point, had no idea of my criminal activities,\n was horrified to learn what I had done, why Hank had taken our children. We were scared. I was in\n Hell, I hated myself for what I had brought upon my family. Recently, I tried once again to quit, to end\n this nightmare, and in response, he gave me this. I can't take this anymore. I live in fear every day that\n Hank will kill me, or worse, hurt my family. I... All I could think to do was to make this video in hope\n that the world will finally see this man, for what he really is.") 
     elif com == "1 custom":
-        pass
+        print("Enter a name")
+        name = input()
+        print("Enter a street address")
+        address = input()
+        print("Enter an adjective")
+        adjective = input()
+        print("Enter a verb ending in -ed")
+        verbed = input()
+        print("Enter a familial relation (i.e. mom, dad, step-sister, brother, etc.)")
+        famrel= input()
+        print("Enter a first name")
+        fname = input()
+        print("Enter a last name")
+        lname = input()
+        print("Enter a noun")
+        noun = input()
+        print("Enter a deterministic pronoun (i.e. his, hers, theirs, etc.)")
+        pronoundet = input()
+        print("Enter the name of a profession or a job title")
+        jobtitle = input()
+        print("Enter the plural form of a number (i.e. 23rd, 95th, 41st, etc.)")
+        pluralnum = input()
+        print("Enter an identifying pronoun (i.e. he, she, they, etc.)")
+        pronounid = input()
+        print(f"Enter the specific field, career or industry related to a/an {jobtitle}")
+        jobfield = input()
+        print("Enter a verb")
+        verb = input()
+        print("Enter a noun")
+        noun2 = input()
+        print("Enter a verb")
+        verb2 = input()
+        print("Enter a noun")
+        noun3= input()
+        print("Enter the name of an organization")
+        organization = input()
+        print("Enter a verb ending in -ed")
+        verbed2 = input()
+        print("Enter an adjective")
+        adjective2 = input()
+        print("Enter the name of a Gender identity")
+        gender = input()
+        print("Enter an adjective")
+        adjective3 = input()
+        print(f"Enter another word for {adjective3}")
+        anoadj = input()
+        print("Enter an adjective")
+        adjective4 = input()
+        print("Enter a verb ending in -ing")
+        verbing = input()
+        print("Enter a signifigant life event (doesn't matter wheather its made up or real shouldn't be more than 2 or 3 words, i.e. prostate exam, colonoscopy, etc. )")
+        lifeevent = input()
+        print("Enter an adjective")
+        adjective5 = input()
+        print("Enter an activity")
+        activity = input()
+        print("Enter a noun")
+        noun4 = input()
+        print("Enter an ajective")
+        adjective6 = input()
+        print("Enter a contraction or a past-tense verb")
+        didordidnt = input()
+        print("Enter an adjective and a verb")
+        adjecverb = input()
+        print ("Enter an emotion")
+        emotion = input()
+        print("Enter an idiom")
+        idiom = input()
+        print("Enter the name of a Gender identity")
+        gender2 = input()
+        print("Enter another first name")
+        fname2 = input()
+        print("Enter another last name")
+        lname2 = input()
+        print("Enter another Profession or Job title")
+        jobtitle2 = input()
+        print("Enter a verb ending in -ed")
+        verbed3 = input()
+        print("Enter a noun")
+        noun5 = input()
+        print("Enter a verb")
+        verb3 = input()
+        print ("Enter a verb ending in -ed")
+        verbed4 = input()
+        print("Enter a noun")
+        noun6 = input()
+        print(f"Enter a noun relating to a/an {jobtitle2}")
+        jobnoun = input()
+        print("Enter a pronoun that is the object of a verb or preposition (i.e. him, her, them, etc.")
+        pronounobj = input()
+        print("Enter a verb in quotes")
+        verb4 = input()
+        print("Enter an adverb")
+        adverb = input()
+        print("Enter an adjective")
+        adjective7 = input()
+        print("Enter a verb ending in -ing")
+        verbing2 = input()
+        print("Enter a noun")
+        noun7 = input()
+        print(f"Enter the result of {verbing2} someones {noun7}")
+        effect = input()
+
