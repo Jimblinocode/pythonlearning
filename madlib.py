aec15d6d018f66695f7e3afc3229193cc1aa0a76
jimblinocode <gorillaking36@gmail.com>
2023-01-17 22:34:58 -0600

    i started the thing

---
+++ madlib.py
@@ -0,0 +1,46 @@
+# print("\tpee pee poo poo")
+# print(f"My name is {Name1}. I live at {Address}.\n This is my confession. If you're watching this tape, I'm probably {lifestate}, {verbed} by my {familialrelation},\n {fName} {lName}. {fname} has been building a {noun} empire for over a year now and using me as his\n {jobtitle}. Shortly after my {ageplural} birthday, {fname} came to me with a rather, shocking proposition. He\n asked that I use my {jobtitle} knowledge to {adjective} {noun2}, which he would then {verb} using\n his connections in the {rnoun} world. Connections that he made through his career with the {organization}. I was...\n {emotiveverb}, I... I always thought that {fname} was a very {adjective2} {Gender} and I was... {ano-adjective}, {adjective3}, but I\n was also particularly {adjective4} at the time, something he knew and took advantage of. I was {verbing}\n from a {life-event} that was poised to {adjective5} my family. {fname} took me on a {activity}, and\n showed me just how much {noun3} even a small {noun} operation could make. And I was {adjective6}. I {didordidnt}\n want my family to {potential-event} so I agreed. Every day, I think back at that moment with {emotion}.\n I quickly realized that I was {idiom}, and {fname} had a partner, a man named {fname2}\n {lname2}, a {jobtitle2}. {fname} essentially {verbed2} me into {noun4} to this {Gender2}, and when I tried to quit,\n Fring threatened my family. I didn't know where to turn. Eventually, Hank and Fring had a falling out.\n From what I can gather, Hank was always pushing for a greater share of the business, to which Fring\n flatly refused to give him, and things escalated. Fring was able to arrange, uh I guess I guess you call it\n a hit on my brother-in-law, and failed, but Hank was seriously injured, and I wound up paying his\n medical bills which amounted to a little over $177,000. Upon recovery, Hank was bent on revenge,\n working with a man named Hector Salamanca, he plotted to kill Fring, and did so. In fact, the bomb\n that he used was built by me, and he gave me no option in it. I have often contemplated suicide, but\n I'm a coward. I wanted to go to the police, but I was frightened. Hank had risen in the ranks to become\n the head of the Albuquerque DEA, and about that time, to keep me in line, he took my children from\n me. For 3 months he kept them. My wife, who up until that point, had no idea of my criminal activities,\n was horrified to learn what I had done, why Hank had taken our children. We were scared. I was in\n Hell, I hated myself for what I had brought upon my family. Recently, I tried once again to quit, to end\n this nightmare, and in response, he gave me this. I can't take this anymore. I live in fear every day that\n Hank will kill me, or worse, hurt my family. I... All I could think to do was to make this video in hope\n that the world will finally see this man, for what he really is."")
+# variables[
+    # Name1
+    # Address
+    # lifestate
+    # verbed (1,2)
+    # familialrelation
+    # fname (1,2)
+    # lname (1,2)
+    # noun (1,2,3)
+    # jobtitle (1,2)
+    # ageplural
+    # adjective (1,2,3,4,5,6) 
+    # verb (1)
+    # rnoun
+    # organization
+    # emotiveverb
+    # gender (1,2)
+    # ano-adjective
+    # verbing
+    # life-event
+    # activity
+    # didordidn't
+    # potential-event
+    # emotion
+    # idiom
+    # ]
+options = ["1. Walter White confession tape", "2. Vaporeon Copypasta", "3. Navy Seal Copypasta",]
+
+print("Welcome to copypasta generator! which copypasta would you like to generate?")
+
+for item in options:
+    print(item)
+
+print("if you want to madlib the copypasta, type *number* custom (*number* meaning the number corresponding to the options on screen)")
+print("if you want to just display the copypasta as is, type *number* default or just *number*")
+
+com: str = input()
+running = True
+
+while running:
+    if com == "1 default" or "1":
+        print(" My name is Walter Hartwell White. I live at 308 Negra Arroyo Lane, Albuquerque, New Mexico, 87104.\n This is my confession. If you're watching this tape, I'm probably dead, murdered by my brother-in-law\n Hank Schrader. Hank has been building a meth empire for over a year now and using me as his\n chemist. Shortly after my 50th birthday, Hank came to me with a rather, shocking proposition. He\n asked that I use my chemistry knowledge to cook methamphetamine, which he would then sell using\n his connections in the drug world. Connections that he made through his career with the DEA. I was...\n astounded, I... I always thought that Hank was a very moral man and I was... thrown, confused, but I\n was also particularly vulnerable at the time, something he knew and took advantage of. I was reeling\n from a cancer diagnosis that was poised to bankrupt my family. Hank took me on a ride along, and\n showed me just how much money even a small meth operation could make. And I was weak. I didn't\n want my family to go into financial ruin so I agreed. Every day, I think back at that moment with regret.\n I quickly realized that I was in way over my head, and Hank had a partner, a man named Gustavo\n Fring, a businessman. Hank essentially sold me into servitude to this man, and when I tried to quit,\n Fring threatened my family. I didn't know where to turn. Eventually, Hank and Fring had a falling out.\n From what I can gather, Hank was always pushing for a greater share of the business, to which Fring\n flatly refused to give him, and things escalated. Fring was able to arrange, uh I guess I guess you call it\n a hit on my brother-in-law, and failed, but Hank was seriously injured, and I wound up paying his\n medical bills which amounted to a little over $177,000. Upon recovery, Hank was bent on revenge,\n working with a man named Hector Salamanca, he plotted to kill Fring, and did so. In fact, the bomb\n that he used was built by me, and he gave me no option in it. I have often contemplated suicide, but\n I'm a coward. I wanted to go to the police, but I was frightened. Hank had risen in the ranks to become\n the head of the Albuquerque DEA, and about that time, to keep me in line, he took my children from\n me. For 3 months he kept them. My wife, who up until that point, had no idea of my criminal activities,\n was horrified to learn what I had done, why Hank had taken our children. We were scared. I was in\n Hell, I hated myself for what I had brought upon my family. Recently, I tried once again to quit, to end\n this nightmare, and in response, he gave me this. I can't take this anymore. I live in fear every day that\n Hank will kill me, or worse, hurt my family. I... All I could think to do was to make this video in hope\n that the world will finally see this man, for what he really is.") 
+    elif com == "1 custom":
+        pass
