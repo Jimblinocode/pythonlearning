050c9e91d1f6913693be5be75090fb67d152c974
jimblinocode <gorillaking36@gmail.com>
2023-01-22 19:02:16 -0600

    blah

---
+++ singlemadlib.py
@@ -0,0 +1,33 @@
+# text
+#  > I was only {age} years old. 
+#  > I loved {name} so much, I {verbed} all the {nounrelatedname} and {nounrelatedname2}. 
+#  > I'd pray to {name} every night before I go to bed, thanking {pronoun2} for the life I've been given. 
+#  > "{name} is love", I would say, "{name} is life". 
+#  > My {relative} hears me and calls me a/an {insult}. 
+#  > I knew {pronoun1relative} was just jealous for my devotion for {name}. 
+#  > I called him a {insult2}. 
+#  > He slaps me and sends me to go to sleep. 
+#  > I'm crying now and my face hurts. 
+#  > I lay in bed and it's really cold. 
+#  > A warmth is moving towards me. 
+#  > I feel something touch me. 
+#  > It's {name}. 
+#  > I'm so happy. 
+#  > {pronoun1} whispers in my ear, "This is my {nounrelatedname}". 
+#  > {pronoun1} grabs me with {pronoun3} powerful ogre hands, and puts me on my hands and knees.
+#  > i'm ready
+#  > I spread my {openablething} for {name}. 
+#  > {pronoun2} {verbs} my {openablethinginside}. 
+#  > It hurts so much, but I do it for {name}. 
+#  > I can feel my {shorthandopenablething} {verbing} as my eyes start to water. 
+#  > I {verb} against his {noun}. 
+#  > I want to please {name}. 
+#  > He roars a mighty roar, as he fills my butt with his love. 
+#  > My dad walks in. 
+#  > Shrek looks him straight in the eye, and says, "It's all ogre now". 
+#  > Shrek leaves through my window. 
+#  > Shrek is love. Shrek is life.
+
+# pronoun guide
+    # he(1)/him(2)/his(3)
+    # "he" would be pronoun 1, "him" would be pronoun 2 and "his" would be pronoun 3. same order goes for she/her/hers, they/them/theirs as well as any other unspecified pronouns.
