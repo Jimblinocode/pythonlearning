6ae04e4dd385d043c7f74a1e6cb012b9c8b2c594
jimblinocode <gorillaking36@gmail.com>
2023-01-24 23:08:00 -0600

    challenge started

---
+++ practice13.py
@@ -0,0 +1,27 @@
+com = input()
+
+def reverse(com:str):
+    # string "hello!"
+    for item in com:
+        item += ""
+        print(item, end="")
+    return 
+  
+        
+    
+    
+
+def ispalindrome(com:str):
+    pass
+
+reverse(com)
+
+
+# print("h" + "")
+
+
+# reverse(com)
+
+# com = "hello"
+
+# print(com[4] + com[3] + com[2] + com[1] + com[0])
