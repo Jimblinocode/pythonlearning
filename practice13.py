499e64d6459667471210c8981bdd0765259631ff
jimblinocode <gorillaking36@gmail.com>
2023-01-27 23:36:02 -0600

    i finally fuckin did it

--- practice13.py
+++ practice13.py
@@ -1,3 +1,32 @@
+
+# both of these are the same, the main difference is that with the top one, you will know which index your currently on. this is because len(text) counts the number of characters while range() creates an iterable
+
+
+# for i in range(len(string)):
+    # print(text[i])
+
+# for item in string:
+    # print(item)
+
+
+
+# index slicing:
+
+# you can access a subset of your iterable by typing myiter[0:2:2], the first number is the index it starts on, the middle number is the index it ends and skips over and the last number is how it counts the index
+
+# text = "Hello!"
+
+# text[0:3] # -> "Hel"
+
+
+
+
+
+
+
+
+
+
 com = input()
 
 def reverse(com:str):
@@ -11,16 +40,14 @@ def reverse(com:str):
     
     
 
-def ispalindrome(com:str):
-        if reverse(com) == com:
-            return bool (True)
-        else:
-            return bool (False)
+def ispalindrome(com:str)-> bool:
+        return reverse(com) == com
+        
 
 
 # print(reverse(com))
 
-print(ispalindrome(com))
+# print(ispalindrome(com))
 
 
 # print("h" + "")
@@ -34,4 +61,31 @@ print(ispalindrome(com))
     # print("maybe")
 # else:
     # print("no")
+
+
+# example string: "amogus! sussy balls"
+
+#challenge 4
+
+
+def reversemeta(com:str) -> str:
+    final = ""
+    start = 0
+    for i in range(len(com)):
+        if not com[i].isalnum():
+            end = i
+            final += reverse(com[start:end])
+            final += com[i]
+            start = i + 1
+    return(final)
+
+print(reversemeta(com))
+
+
+# for i in range(len(com)):
+    # print(com[i])
+
+
+
+
 
