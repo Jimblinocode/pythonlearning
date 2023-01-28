1760ebe22063e78bdaff406564640013f34b51c6
jimblinocode <gorillaking36@gmail.com>
2023-01-27 18:37:49 -0600

    E

--- practice13.py
+++ practice13.py
@@ -2,26 +2,36 @@ com = input()
 
 def reverse(com:str):
     # string "hello!"
+    rev = ""
     for item in com:
-        item += ""
-        print(item, end="")
-    return 
+        rev = item + rev
+    return (rev)
   
         
     
     
 
 def ispalindrome(com:str):
-    pass
+        if reverse(com) == com:
+            return bool (True)
+        else:
+            return bool (False)
 
-reverse(com)
+
+# print(reverse(com))
+
+print(ispalindrome(com))
 
 
 # print("h" + "")
 
 
-# reverse(com)
-
 # com = "hello"
 
 # print(com[4] + com[3] + com[2] + com[1] + com[0])
+
+# if reverse(com) == "kayak":
+    # print("maybe")
+# else:
+    # print("no")
+
