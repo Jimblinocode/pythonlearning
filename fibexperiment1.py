9b1663d7dbffe03465befe098e6ce419acd5508b
jimblinocode <gorillaking36@gmail.com>
2023-02-04 21:38:55 -0600

    basically figured out how to reach maximum recursion depth instantly

---
+++ fibexperiment1.py
@@ -0,0 +1,17 @@
+# an experiment to see what would happen if i iterated a recursive fibonnacci function and an incrementing variable through a while loop
+
+
+
+def fib(n) -> int:
+    if n == 1:
+        return(0)
+    elif n == 2:
+        return(1)
+    return fib(n - 1) + fib(n - 2)
+
+running = True
+
+while running:
+    num = 0
+    fib(num)
+    num += 1
