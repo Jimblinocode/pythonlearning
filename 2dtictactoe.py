9e315c19f34d5373784116c0ca83b34e5ad0892a
jimblinocode <gorillaking36@gmail.com>
2023-02-07 21:01:50 -0600

    p

--- 2dtictactoe.py
+++ 2dtictactoe.py
@@ -15,8 +15,12 @@ tictactoe = [
 
 running = True
 
+for item in tictactoe:
+    print(item)
+
 while running:
-    print("Player 1 (X) please enter 2 numbers between 0 and 2 for a row and a coulumn")
+    # P1 input
+    print("Player 1 (X) please enter 2 numbers, both between 0 and 2 for a row and a coulumn")
     num1 = int(input())
     num2 = int(input())
     if tictactoe[num1][num2] == "O":
@@ -28,12 +32,42 @@ while running:
         for item in tictactoe:
             print(item)
     
+    # horizontal win - P1
+
     if tictactoe[0][0] == "X" and tictactoe[0][1] == "X" and tictactoe[0][2] == "X":
+        print("PLAYER 1 WINS")
+        break
+    elif tictactoe[1][0] == "X" and tictactoe[1][1] == "X" and tictactoe[1][2] == "X":
+        print("PLAYER 1 WINS")
+        break
+    elif tictactoe[2][0] == "X" and tictactoe[2][1] == "X" and tictactoe[2][2] == "X":
+        print("PLAYER 1 WINS")
+        break
+
+    # vertical win - P1
+    if tictactoe[0][0] == "X" and tictactoe[1][0] == "X" and tictactoe[2][0] == "X":
+        print("PLAYER 1 WINS")
+        break
+    elif tictactoe[0][1] == "X" and tictactoe[1][1] == "X" and tictactoe[2][1] == "X":
+        print("PLAYER 1 WINS")
+        break
+    elif tictactoe[0][2] == "X" and tictactoe[1][2] == "X" and tictactoe[2][2] == "X":
+        print("PLAYER 1 WINS")
+        break
+    
+    # diagonal win - P1
+    if tictactoe[0][0] == "X" and tictactoe[1][1] == "X" and tictactoe[2][2] == "X":
+        print("PLAYER 1 WINS")
+        break
+    elif tictactoe[0][2] == "X" and tictactoe[1][1] == "X" and tictactoe[2][0] == "X":
         print("PLAYER 1 WINS")
         break
 
 
-    print("Player 2 (O) please enter 2 numbers between 0 and 2 for a row and a column ")
+
+
+    # P2 input
+    print("Player 2 (O) please enter 2 numbers, both between 0 and 2 for a row and a column ")
     num1 = int(input())
     num2 = int(input())
     if tictactoe[num1][num2] == "X":
@@ -45,9 +79,34 @@ while running:
         for item in tictactoe:
             print(item)
     
+    # horizontal win - P2
+
     if tictactoe[0][0] == "O" and tictactoe[0][1] == "O" and tictactoe[0][2] == "O":
         print("PLAYER 2 WINS")
         break
-   
+    elif tictactoe[1][0] == "O" and tictactoe[1][1] == "O" and tictactoe[1][2] == "O":
+        print("PLAYER 2 WINS")
+        break
+    elif tictactoe[2][0] == "O" and tictactoe[2][1] == "O" and tictactoe[2][2] == "O":
+        print("PLAYER 2 WINS")
+        break
+    
+    # vertical win - P2
 
-
+    if tictactoe[0][0] == "O" and tictactoe[1][0] == "O" and tictactoe[2][0] == "O":
+        print("PLAYER 2 WINS")
+        break
+    elif tictactoe[0][1] == "O" and tictactoe[1][1] == "O" and tictactoe[2][1] == "O":
+        print("PLAYER 2 WINS")
+        break
+    elif tictactoe[0][2] == "O" and tictactoe[1][2] == "O" and tictactoe[2][2] == "O":
+        print("PLAYER 2 WINS")
+        break
+    
+    # diagonal win - P2
+    if tictactoe[0][0] == "O" and tictactoe[1][1] == "O" and tictactoe[2][2] == "O":
+        print("PLAYER 2 WINS")
+        break
+    elif tictactoe[0][2] == "O" and tictactoe[1][1] == "O" and tictactoe[2][0] == "O":
+        print("PLAYER 2 WINS")
+        break
