e411558087b1df99ed5b403bc22c04794f8a81d0
jimblinocode <gorillaking36@gmail.com>
2023-02-09 14:21:47 -0600

    started

--- tictactoeredux.py
+++ tictactoeredux.py
@@ -1,4 +1,28 @@
+from os import system
 # grid generation
-grid = [[" " for i in range(3)]for i in range(3)]
+grid = [["X" for i in range(3)]for i in range(3)]
 
-print(grid[0])
+def gridgen(grid):
+    for row in grid:
+        for item in row:
+            print(item, end="")
+        print("")
+    return(grid)
+
+title = [
+    ["T", "I", "C"],
+    ["A", " ", " "],
+    ["C", " ", " "],
+    ["T", "O", "E"]
+]
+
+def titleprint(title):
+    for row in title:
+        for item in row:
+            print(item, end="")
+        print("")
+    return (title)
+
+
+titleprint(title)
+gridgen(grid)
