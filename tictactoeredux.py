938897e8124c1da8ef6136c7be0d378ff774938e
jimblinocode <gorillaking36@gmail.com>
2023-02-09 19:27:00 -0600

    still working on it

--- tictactoeredux.py
+++ tictactoeredux.py
@@ -1,6 +1,6 @@
 from os import system
 # grid generation
-grid = [["X" for i in range(3)]for i in range(3)]
+grid = [[" " for i in range(3)]for i in range(3)]
 
 def gridgen(grid):
     for row in grid:
@@ -9,20 +9,36 @@ def gridgen(grid):
         print("")
     return(grid)
 
-title = [
-    ["T", "I", "C"],
-    ["A", " ", " "],
-    ["C", " ", " "],
-    ["T", "O", "E"]
-]
-
-def titleprint(title):
-    for row in title:
-        for item in row:
-            print(item, end="")
-        print("")
-    return (title)
 
 
-titleprint(title)
+
+print("T I C T A C T O E")
 gridgen(grid)
+
+# Player controller
+playernum = 0
+player = ("X", "O", "@")
+
+def gameplayloop():
+    global playernum
+    global player
+    global cls
+    while True:
+        try:   
+            print(f"Player {playernum + 1}, please enter 2 numbers between 0 and 2, the first one will represent a row, the second will represent the column ")
+            n1, n2 = (int(input()), int(input()))
+            if grid[n1][n2] == player[0] or grid[n1][n2] == player[1] or grid[n1][n2] == player[2]:
+                print("SPOT TAKEN, PLEASE ENTER DIFFERENT COORDINATES")
+            else:
+                grid[n1][n2] = player[playernum]
+                gridgen(grid)
+                playernum += 1
+                player[playernum] %= 2
+                system(cls)
+
+        except:
+            print("INVALID CHARACTER, PLEASE ENTER A VALID NUMBER")
+            n1, n2 = gameplayloop()
+
+
+gameplayloop()
