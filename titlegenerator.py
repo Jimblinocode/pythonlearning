232e70e7de941423d73b77b23fd9a0781fd288ab
jimblinocode <gorillaking36@gmail.com>
2023-02-18 16:25:48 -0600

    p

--- titlegenerator.py
+++ titlegenerator.py
@@ -12,12 +12,14 @@ def videoessaygenerator():
         videoessaytitleB = ["the problem with", "in defense of", f"the {blank}ification of", "the rise and fall of", ]
         prefix = videoessaytitleB[randint(0,3)]
 
-        issue = ["forced diversity", "tiktok", "AI art", "smosh", "masturbation", "porn", "velma", "pedophillia", "beastiality", "necrophilia", "channel awesome", "creepshow art", "yandere dev", "keffals", "debate bros", "nikocado avocado", "mrbeast"]
-        subject = issue[randint(0,16)]
+        issue = ["forced diversity", "tiktok", "AI art", "smosh", "masturbation", "porn", "velma", "pedophillia", "beastiality", "necrophilia", "channel awesome", "creepshow art", "yandere dev", "keffals", "debate bros", "nikocado avocado", "mrbeast", "pedophiles", "zoophiles", "vaush", "necrophiles"]
+        subject = issue[randint(0,20)]
         
         compositestatement = f"{prefix} {subject}"
 
         return(compositestatement)
 
 
-print(videoessaygenerator())
+while True:
+    com = input()
+    print(videoessaygenerator())
