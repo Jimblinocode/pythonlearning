742acb3a652a5f5700b5114f6589c00be2d97eb4
jimblinocode <gorillaking36@gmail.com>
2023-02-18 12:18:39 -0600

    made a video essay title generator. literally so easy, might use in a bot someday

---
+++ titlegenerator.py
@@ -0,0 +1,23 @@
+from random import randint
+
+
+
+
+
+def videoessaygenerator():
+    while True:
+        blankification = ["yass", "mrbeast", "anime", "fly", "vaush"]
+        blank = blankification[randint(0,4)]
+
+        videoessaytitleB = ["the problem with", "in defense of", f"the {blank}ification of", "the rise and fall of", ]
+        prefix = videoessaytitleB[randint(0,3)]
+
+        issue = ["forced diversity", "tiktok", "AI art", "smosh", "masturbation", "porn", "velma", "pedophillia", "beastiality", "necrophilia", "channel awesome", "creepshow art", "yandere dev", "keffals", "debate bros", "nikocado avocado", "mrbeast"]
+        subject = issue[randint(0,16)]
+        
+        compositestatement = f"{prefix} {subject}"
+
+        return(compositestatement)
+
+
+print(videoessaygenerator())
