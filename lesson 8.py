8cacddb54dc2151c3f996549349cf9275b4b10ad
jimblinocode <gorillaking36@gmail.com>
2023-02-24 14:07:42 -0600

    p

---
+++ lesson 8.py
@@ -0,0 +1,28 @@
+# every object is associated with a type
+
+# identifiers (variables, function names) are binded to objects, we call those binds
+
+# ex: var = 7 would be:
+
+# var binds to 7 which is an instance of int
+
+# bindings can also be rebinded
+
+# type is an instance of type 
+
+# every class is an object of type type
+
+# defining your own classes
+
+# class peepeepoopoo:
+    # pass
+
+# in your class you can add any code, however any bindings will become attributes of the class
+
+# dir dir (directory) if you want a list of attributes of an object
+
+# when you call a function as a method of the object of a class that object is taken as a positional arguement
+
+# custom classes are mutable and can be changed, same with objects created within those functions, however you can't change the attributes of objects python gives to you by default
+
+# the self parameter: a parameter that just represents the object within your class
