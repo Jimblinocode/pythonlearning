6514e08050340d9281b074904e5a593bbc4b3207
jimblinocode <gorillaking36@gmail.com>
2023-04-08 23:09:15 -0500

    p

---
+++ lesson X3.py
@@ -0,0 +1,5 @@
+# encapsulation. cordoning off your code into a specific namespace.
+    # private: member (method/attribute) is only availible within a specific class namespace
+    # protected: member is only availible with a specific class and the subclasses of said class
+    # public: the default usage of members, can be used anywhere in the script
+# 
