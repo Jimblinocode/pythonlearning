90ede3f82a031b5aab9ce168c7ce30815a42466b
jimblinocode <gorillaking36@gmail.com>
2023-04-16 20:31:18 -0500

    p

--- diceroller.py
+++ diceroller.py
@@ -1,15 +1,14 @@
 import random
 print("roll a D20!")
-while True:
-    i = 0
-    com = input()
-    if com == "roll" or com == "ROLL":
-        while i < 2:
-            thingy = random.randint(0,20)
-            print(thingy)
-            if thingy == 20:
-                break
-            else:
-                i += 2
+com = input()
+i = 0
+if com == "roll" or com == "ROLL":
+    while i < 2:
+        thingy = random.randint(0,20)
+        print(thingy)
+        if thingy == 20:
+            break
+        else:
+            i += 1
 
             
