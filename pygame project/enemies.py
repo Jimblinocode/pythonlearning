9d77dc89d7d7c26e8daa1cb9399d547056b01e08
jimblinocode <gorillaking36@gmail.com>
2024-02-10 18:01:08 -0600

    p

--- pygame project/enemies.py
+++ pygame project/enemies.py
@@ -1,2 +1,22 @@
 import random
+import globals
 import pygame
+from colors import *
+
+class enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(enemy, self).__init__()
+        self.position = [random.randint(0, globals.res[0]), random.randint(0, globals.res[1])]
+        self.sizze = [100, 100]
+        self.image = pygame.Surface(self.sizze)
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect(center = self.position)
+        self.speed = 10
+
+    def update(self):
+        if self.rect.right < 0:
+            self.kill()
+
+
+    def Draw(self):
+        globals.screen.blit(self.image, self.rect)
--- pygame project/gameloop.py
+++ pygame project/gameloop.py
@@ -2,12 +2,14 @@ import globals
 from player import Player
 import colors
 import pygame
+from enemies import enemy
 
 class game:
     def __init__(self):
         self.clock = pygame.time.Clock()
         self.fps = 165
         self.player = Player()
+        self.Enemy = enemy()
 
     def process_input(self):
         for event in pygame.event.get():
@@ -18,10 +20,12 @@ class game:
 
     def update(self):
         self.player.update()
+        self.Enemy.update()
 
     def draw(self):
         globals.screen.fill(colors.GREY)
         self.player.draw()
+        self.Enemy.Draw()
         pygame.display.flip()    
 
     def run(self):
